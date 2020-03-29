a53eec85681c34c0366b948078084d26dcbd6da7
gpsyrou <gspyrou@umich.edu>
2020-03-29 13:32:48 +0100

    delete all files that did not contain relevant data;

--- dataCleaning.py
+++ dataCleaning.py
@@ -19,14 +19,34 @@ import pandas as pd
 
 # Relative folder path to the executable dataCleaning.py file
 dirname = os.path.dirname(__file__)
-csv_loc = 'C:\\Users\\george\\Desktop\\GitHub\\Projects\\Comtrade_Network\\CSVFiles'
 csv_loc = os.path.join(dirname, 'CSVFiles')
 
-def sniffValidCsv(csv_name: str, csv_loc: str) -> None:
-    sniffDf = pd.read_csv(csv_name, delimiter=',', nrows=1, header=[0])
-    if sniffDf['Classification'][0] != 'HS':
-        print(f'The file {csv_name} does not contain valid data! Deleting file from directory..\n')
-        os.remove(os.path.join(csv_loc, csv_name))
-    else:
-        pass
+def sniffValidCSV(csv_name: str, csv_folder: str) -> None:
+    '''
+    Sniff a csv file to check if it contains valid data.
+    The csv files that are not valid will not have a string 'HS' as their first value of the 'Classification' column.
 
+    Args:
+    ----
+        csv_name: Path to the csv_file that needs checking.
+        csv_folder: Path to the folder that contains all the csv files.
+    '''
+    try:
+        sniffDf = pd.read_csv(os.path.join(csv_folder, csv_name), delimiter=',', nrows=1, header=[0])
+        try:
+            if sniffDf['Classification'][0] != 'HS':
+                print(f'The file {csv_name} does not contain valid data! Deleting file from directory..\n')
+                os.remove(os.path.join(csv_folder, csv_name))
+            else:
+                print(f'File {csv_name} contained data..\n')
+                pass
+        except KeyError:
+                print(f'The file {csv_name} is not a possible argument combination..\n')
+                os.remove(os.path.join(csv_folder, csv_name))
+    except OSError:
+        print(f'Bad formated file: {csv_name}')
+
+
+# Run the process to delete the files that do not contain relevant data for our analysis
+for file in os.listdir(csv_loc):
+    sniffValidCSV(file, csv_loc)
