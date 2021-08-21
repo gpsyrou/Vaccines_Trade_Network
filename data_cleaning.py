9eeb3db4fc321a75293bd707b6d98eb18997acfd
gpsyrou <gspyrou@umich.edu>
2021-08-21 20:48:56 +0100

    apply pep8: data_cleaning.py and data_retrieval.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,29 +2,27 @@
 -------------------------------------------------------------------
 -- Title:
 -- File:    cleanData.py
--- Purpose: Data cleaning and processing for the CSV files as received from the data retrieval process (getData.py).
-            The script deletes the files that do not contain data, and merges all the rest csv to a unique one.
+-- Purpose: Data cleaning and processing for the CSV files as received from 
+            the data retrieval process (getData.py).
+            The script deletes the files that do not contain data, and merges
+            all the rest csv to a unique one.
 -- Author:  Georgios Spyrou
 -- Date:    29/03/2020
 -------------------------------------------------------------------
 """
 
-# Import dependencies
 import os
-import sys
-import csv
 import pandas as pd
 import argparse
 
-# Some files received contain no data, as no recorded data exist for all countries.
-# Before we merge the files to a unique file that will contain the clean data,
-# we will delete the files that contain no relevant data.
-
-# Relative folder path to the executable dataCleaning.py file
 dirname = os.path.dirname(__file__)
 
 parser = argparse.ArgumentParser(description='Parses for year of interest')
-parser.add_argument('year', type=str, help='A required integer positional argumentn defining year of interest')
+parser.add_argument(
+    'year',
+    type=str,
+    help='A required integer positional argumentn defining year of interest'
+    )
 args = parser.parse_args()
 
 csv_loc = os.path.join(dirname, 'CSVFiles', args.year)
@@ -33,28 +31,33 @@ csv_loc = os.path.join(dirname, 'CSVFiles', args.year)
 def valid_csv_sniffer(csv_name: str, csv_folder: str) -> None:
     """
     Check a CSV file to understand if it contains valid data.
-    The CSV files that are not valid will not have a string 'HS' as their first value of the 'Classification' column.
+    The CSV files that are not valid will not have a string 'HS' as their
+    first value of the 'Classification' column.
 
     Args:
     ----
         csv_name: Path to the csv_file that needs checking.
         csv_folder: Path to the folder that contains all the csv files.
     """
+    csv_file = os.path.join(csv_folder, csv_name)
     try:
-        sniffed_df = pd.read_csv(os.path.join(csv_folder, csv_name), delimiter=',', nrows=1, header=[0])
+        sniffed_df = pd.read_csv(csv_file, delimiter=',', nrows=1, header=[0])
         try:
             if sniffed_df['Classification'][0] != 'HS':
-                print(f'The file {csv_name} does not contain valid data! Deleting file from directory..\n')
-                os.remove(os.path.join(csv_folder, csv_name))
+                print(
+                    f'The file {csv_name} does not contain valid data!\n'
+                    f'Deleting file from directory..\n'
+                    )
+                os.remove(csv_file)
             else:
                 print(f'File {csv_name} contained data..\n')
                 pass
         except KeyError:
-                print(f'The file {csv_name} is not a possible argument combination..\n')
-                os.remove(os.path.join(csv_folder, csv_name))
+                print(f'{csv_name} is not a possible argument combination..\n')
+                os.remove(csv_file)
     except OSError:
         print(f'Badly formated file: {csv_name}')
-        os.remove(os.path.join(csv_folder, csv_name))
+        os.remove(csv_file)
 
 
 def read_dataframe(filepath):
@@ -65,15 +68,19 @@ def read_dataframe(filepath):
     return df
 
 
-# Run the process to delete the files that do not contain relevant data for our analysis
+# Delete the files that do not contain relevant data for our analysis
 for file in os.listdir(csv_loc):
     valid_csv_sniffer(file, csv_loc)
 
 # Merge the clean CSV files to a unique csv file.
-combined_csvs = pd.concat([read_dataframe(os.path.join(csv_loc, f)) for f in os.listdir(csv_loc)])
+combined_csvs = pd.concat(
+    [read_dataframe(os.path.join(csv_loc, f)) for f in os.listdir(csv_loc)]
+    )
 
 # Create a csv file
 if not os.path.exists(os.path.join(dirname, 'Merged_CSVs')):
     os.mkdir(os.path.join(dirname, 'Merged_CSVs'))
 
-combined_csvs.to_csv(f"Merged_CSVs\\Comtrade_Vacciness_Data_{args.year}", index=False)
+combined_csvs.to_csv(
+    f"Merged_CSVs\\Comtrade_Vacciness_Data_{args.year}", index=False
+    )
--- data_retrieval.py
+++ data_retrieval.py
@@ -1,14 +1,13 @@
 """
 -------------------------------------------------------------------
--- Title:
--- File:    dataRetrieval.py
--- Purpose: Gather monthly data for multiple years for the imports/exports of Vaccines products around the world, from  https://comtrade.un.org/Data/
+-- File:    data_cleaning.py
+-- Purpose: Gather monthly data for multiple years for the imports/exports
+            of a service/commodity from https://comtrade.un.org/Data/
 -- Author:  Georgios Spyrou
--- Date:    28/03/2020
+-- Last Updated Date: 21/08/2021
 -------------------------------------------------------------------
 """
 
-# Import dependencies
 from typing import List
 import requests
 import csv
@@ -17,12 +16,15 @@ import time
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='Provided list of years to retrieve data for')
+parser = argparse.ArgumentParser(
+    description='Provided list of years to retrieve data for'
+    )
 
 # Provide as input a list of years
 parser.add_argument('-years', '--arg', nargs='+', type=int, dest='years')
 args = parser.parse_args()
-print('List of years provided {}\n\n Initiating data retrieval process...\n\n'.format(args.years))
+print('List of years provided {}\n\n'.format(args.years))
+print('Initiating data retrieval process...\n\n')
 
 # Get the data as separate csv files, each for every year of interest
 outputFilesFolder = f'CSVFiles\\'
@@ -37,25 +39,32 @@ frequency = 'M'             # Monthly
 px = 'HS'                   # Classification for products
 cc = 300220                 # Subcategory --> 300220 code for Vaccines
 reporter = 'all'
-partner = 'all'                 # world
+partner = 'all'             # World
 rg ='all'
 
 # Connection string to comtrade.un.org based on the parameters above
 api_call_string = f'http://comtrade.un.org/api/get?max={max_rec}&type={trade_type}&freq={frequency}&px={px}&ps=year&r=reporter&p={partner}&rg={rg}&cc={cc}&fmt={output_fmt}'
 
 
-def collect_data(api_string: str, reporterid: str, reportername: str, year: int, out_folder: str) -> None:
+def collect_data(
+    api_string: str,
+    reporterid: str,
+    reportername: str,
+    year: int, 
+    out_folder: str) -> None:
     """
-    Create a CSV file that contains the monthly data as received from  https://comtrade.un.org/Data/, for a specified year.
+    Create a CSV file that contains the monthly data as received 
+    from  https://comtrade.un.org/Data/, for a specified year.
 
     Args:
     ----
-        api_string: String that contains the URL for the API call. The string already contains all the paremeters required for the call.
+        api_string: String that contains the URL for the API call. 
         reporterid, reportername: Id and Name of the country of interest.
         year: Specify year of interest.
     Returns:
     -------
-        None: The output is a .csv file that contains the data for a specified country/year.
+        The output is a .csv file that contains the data for a specified 
+        country/year.
     """
     csv_by_year_out_loc = os.path.join(out_folder, f'{year}')
     if not os.path.exists(csv_by_year_out_loc):
@@ -73,17 +82,12 @@ def collect_data(api_string: str, reporterid: str, reportername: str, year: int,
         csv_file = csv.reader(decoded_data.splitlines())
         datalines = list(csv_file)
 
-        with open(os.path.join(csv_by_year_out_loc, f'Comtrade_Vaccines_Data_{reportername}_{year}.csv'), 'w', newline='') as f:
+        filename = f'Comtrade_Vaccines_Data_{reportername}_{year}.csv'
+        filename = os.path.join(csv_by_year_out_loc, filename)
+        with open(filename, 'w', newline='') as f:
             writer = csv.writer(f, delimiter=',')
             writer.writerows(datalines)
 
-
-# Retrieve the data
-
-# Because the API doesn not allow to call multiple countries as a Reporter in one call, we will need to create
-# separate calls for each country.
-
-# Receive the list of countries and their respective IDs as described in https://comtrade.un.org/Data/cache/reporterAreas.json
 
 reporters_url = 'https://comtrade.un.org/Data/cache/partnerAreas.json'
 reporters_resp = requests.get(url=reporters_url, verify=False)
@@ -100,5 +104,11 @@ for api_check, reporter in enumerate(reporters_list):
     print(f'\nCountry..: {countryname}')
     for year in args.years:
         print(f'\nReceiving the data for {year} from https://comtrade.un.org/...\n')
-        collect_data(api_call_string, reporterid=country_id, reportername=countryname, year=year, out_folder=outputFilesFolder)
+        collect_data(
+            api_call_string,
+            reporterid=country_id,
+            reportername=countryname,
+            year=year,
+            out_folder=outputFilesFolder
+            )
         time.sleep(6)
